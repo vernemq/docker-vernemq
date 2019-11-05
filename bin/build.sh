16f80a6931f6a7c287ae53ae40ecf7c152bd6867
Crowbar Z <crowbarz@outlook.com>
2019-11-05 09:39:04 +0000

    Added Dockerfile for 1.9.2+@47bc480 on armv7l (>= Raspberry Pi 2B) on Alpine

--- bin/build.sh
+++ bin/build.sh
@@ -5,6 +5,10 @@ if [ "$1" = "swc" ]
 then
     make swc
     mv _build/swc/rel/vernemq release
+elif [ "$1" = "rpi32" ]
+then
+    make rpi32
+    mv _build/rpi32/rel/vernemq release
 else
     make rel
     mv _build/default/rel/vernemq release
