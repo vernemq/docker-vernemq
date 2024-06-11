01000900f7e60fdc0f6fff47a7de6df4774cbd04
Christian 'Pelle' Pelster <pelle@pelle.io>
2024-06-11 18:35:35 +0200

    extend wait before configuring api key (#388)

--- bin/vernemq.sh
+++ bin/vernemq.sh
@@ -400,8 +400,12 @@ if [ $start_join_cluster  -eq 1 ]; then
     mkdir -p /var/log/vernemq/log
     join_cluster > /var/log/vernemq/log/join_cluster.log &
 fi
+
 if [ -n "$API_KEY" ]; then
-  sleep 10 && echo "Adding API_KEY..." && /vernemq/bin/vmq-admin api-key add key="${API_KEY:-DEFAULT}"
+  sleep 60
+  echo "Adding API_KEY..."
+  vmq-admin api-key add key="${API_KEY}"
   vmq-admin api-key show
 fi
+
 wait $pid
